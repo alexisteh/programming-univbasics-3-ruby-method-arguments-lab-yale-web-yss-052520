46f7725a9c0e8e8bd59694295d04668faeedf7c2
Alexis Teh <wenhui.teh23@gmail.com>
2020-05-08 02:52:08 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,10 +10,11 @@ end
 
 introduction_with_language("Alexis", "ruby")
 
+def introduction_with_language_optional (name, language = "ruby")
+  puts"Hi, my name is #{name} and I am learning to program in #{language}." 
+end 
 
-
-
-
+introduction_with_language_optional("John")
 
 
 
